b7a214ea6e04d073e7b6e5bf5da3f751dfc6de54
Anthonylons <117861643+Anthonylons@users.noreply.github.com>
2023-10-15 19:06:36 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hellos CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+function Human() {
+  render(
+    <div className="container">
+      <div className="row">
+        <button className="btn btn-primary">Add Values</button>
+      </div>
+    </div>
+  );
+}
---
+++ src/Human.js
@@ -0,0 +1,9 @@
+function Human() {
+  render(
+    <div className="container">
+      <div className="row">
+        <button className="btn btn-primary">Add Values</button>
+      </div>
+    </div>
+  );
+}
